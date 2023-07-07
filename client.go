b4ac32fe64b893a0ab3a2d42899af21a8eb2f0e1
Stephen Nelson <47795400+AGS4NO@users.noreply.github.com>
2023-07-07 02:48:58 -0400

    cleanup option parsing, specify fqdn, add additional setup tests (#7)
    This cleans up some option parsing and strictly sets the FQDN and name from the configuration keys.
    
    Our API expects the zone name without the trailing dot.
    In order to construct RRs for record types that can be created at the apex, we need to inject the FQDN into the zone file as our system returns an empty string for the name.
    dns module sets the FQDN from the configuration key
    name is set by removing the trailing . from the FQDN

--- client.go
+++ client.go
@@ -2,6 +2,8 @@ package dnsimple
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/dnsimple/dnsimple-go/dnsimple"
@@ -15,13 +17,14 @@ type dnsimpleClient struct {
 // dnsimpleService is an interface for dnsimpleClient.
 type dnsimpleService interface {
 	getZone(ctx context.Context, accountID string, zoneName string) (*dnsimple.Zone, error)
-	listZoneRecords(ctx context.Context, accountID string, zoneName string, options *dnsimple.ZoneRecordListOptions, maxRetries int) ([]dnsimple.ZoneRecord, error)
+	listZoneRecords(ctx context.Context, accountID string, zoneName string, maxRetries int) ([]dnsimple.ZoneRecord, error)
+	updateZoneStatus(accountID string, apiCaller DNSimpleApiCaller, maxRetries int, status updateZoneStatusRequest) (err error)
 }
 
 // getZone is a wrapper method around `dnsimple.Client.Zones.GetZone`
 // it checks if a zone exists in DNSimple.
 func (c dnsimpleClient) getZone(ctx context.Context, accountID string, zoneName string) (*dnsimple.Zone, error) {
-	response, err := c.Zones.GetZone(ctx, accountID, strings.TrimSuffix(zoneName, "."))
+	response, err := c.Zones.GetZone(ctx, accountID, zoneName)
 	if err != nil {
 		return nil, err
 	}
@@ -30,15 +33,18 @@ func (c dnsimpleClient) getZone(ctx context.Context, accountID string, zoneName 
 
 // listZoneRecords is a wrapper for `dnsimple.Client.Zones.ListRecords`.
 // It fetches and returns all record sets for a zone handling pagination.
-func (c dnsimpleClient) listZoneRecords(ctx context.Context, accountID string, zoneName string, options *dnsimple.ZoneRecordListOptions, maxRetries int) ([]dnsimple.ZoneRecord, error) {
+func (c dnsimpleClient) listZoneRecords(ctx context.Context, accountID string, zoneName string, maxRetries int) ([]dnsimple.ZoneRecord, error) {
 	var rs []dnsimple.ZoneRecord
+
+	listOptions := &dnsimple.ZoneRecordListOptions{}
+	listOptions.PerPage = dnsimple.Int(100)
 
 	// Fetch all records for the zone.
 	for {
 		var response *dnsimple.ZoneRecordsResponse
 		listErr := retryable(maxRetries, func() (listErr error) {
 			// Our API does not expect the zone name to end with a dot.
-			response, listErr = c.Zones.ListRecords(ctx, accountID, strings.TrimSuffix(zoneName, "."), options)
+			response, listErr = c.Zones.ListRecords(ctx, accountID, strings.TrimSuffix(zoneName, "."), listOptions)
 			return
 		})
 		if listErr != nil {
@@ -48,8 +54,26 @@ func (c dnsimpleClient) listZoneRecords(ctx context.Context, accountID string, z
 		if response.Pagination.CurrentPage >= response.Pagination.TotalPages {
 			break
 		}
-		options.Page = dnsimple.Int(response.Pagination.CurrentPage + 1)
+		listOptions.Page = dnsimple.Int(response.Pagination.CurrentPage + 1)
 	}
 
 	return rs, nil
+}
+
+func (c dnsimpleClient) updateZoneStatus(accountID string, apiCaller DNSimpleApiCaller, maxRetries int, status updateZoneStatusRequest) (err error) {
+	statusJson, err := json.Marshal(status)
+	if err != nil {
+		panic(fmt.Errorf("failed to serialise status request: %v", err))
+	}
+	sendStatusError := retryable(maxRetries, func() (err error) {
+		err = apiCaller(
+			fmt.Sprintf("/v2/%s/platform/statuses", accountID),
+			statusJson,
+		)
+		return
+	})
+	if sendStatusError != nil {
+		return sendStatusError
+	}
+	return nil
 }
